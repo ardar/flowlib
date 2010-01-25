20cf18b5a35e81778da55a3364e4546bcf294273
blomman84 <blomman84@62571296-2f41-0410-a94f-a52d0a42408b>
2010-01-25 23:08:47 +0000

    * Started adding files so we can test basic functionality before releasing new version

---
+++ trunk/TestingBeforeRelease/Applications/Adchpp/readme.txt
@@ -0,0 +1,104 @@
+= ADCH++ - A hub software for ADC
+
+== Introduction
+
+ADCH++ is a hub for the ADC network. It implements the
+http://dcplusplus.sf.net/ADC.html[ADC protocol]. The core application is very
+simple, but extensible using plugins. Among the standard plugins there is a
+scripting plugin that allows hub owners to further customize the hub using the
+http://www.lua.org[Lua] scripting language. The core is also exposed as a
+Python module, thus it is possible to use it within a Python application.
+
+== License
+ADCH++ is licensed under the GPL. For details, see license.txt supplied with
+the application. A side effect of the license is that any plugins you write
+must be released under a license compatible with the GPL.
+
+== Download
+The latest version of ADCH++ can be downloaded from
+http://sourceforge.net/projects/adchpp/[SourceForge]. The source code resides
+in http://sourceforge.net/svn/?group_id=172105[SVN].
+
+== Requirements
+To run ADCH++ you will need the following:
+
+* A computer with a network card
+* Linux with a 2.6-based kernel or Windows 2000/XP
+* A brain (to understand the readme and do the setup)
+* Optional: An administrator account (to install as service / run on port < 1024 on
+  unix)
+
+NOTE: The hub will not run on Windows 9x/ME
+
+To compile the sources you will also need (other versions might work but I don't use them):
+
+* GCC 4.4+ (Linux or http://www.mingw.org[MinGW])
+* http://www.scons.org[SCons 0.98.5]
+* http://www.swig.org[SWIG 1.3.39]
+* http://www.python.org[Python] 2.5 (Windows) or 2.4+ (Linux)
+
+== Building
+To build ADCH++ from source you have to:
+
+* Install SWIG and ensure it's in your PATH
+* Install Python and ensure it's in your PATH
+* Install SCons and ensure it's in your PATH
+* Windows: Install MinGW and ensure it's in your PATH
+* Linux: Install GCC and appropriate header files, in particular, these packages spring to mind:
+	** gcc-c++
+	** libstdc++-devel
+	** readline-devel
+* To compile with support for secure connections, you need OpenSSL:
+	** Windows: compile it yourself (or ask someone to do it) and put it in the "openssl" directory
+	** Linux: if you don't have it already, get the openssl-devel package
+* In the source folder, type "scons -h" to see additional compile options
+* Type "scons" to create a debug build. "scons mode=release" will create a
+  release build.
+
+== Configuration
+ADCH++ is configured using an XML file, as are the standard plugins. The
+example configuration contains enough comments to get you started. In Linux,
+the default location for configuration files is "/etc/adchpp/". In Windows, it's
+a directory named "config" under the installation directory.
+
+== Running
+ADCH++ will normally run as a console / terminal application but can also be
+convinced to run in the background (daemon/service). It accepts various
+command line options such as:
+
+[separator="|"]
+``_
+-c <configdir> | Run with an alternate config directoy. Must be an absolute path.
+-i <name>      | Install the hub service to enable running as a service. * Windows only *
+-u <name>      | Remove the service you created earlier. * Windows only *
+-v             | Print version information (make sure to include this in any bug reports)
+-d             | Run as a daemon. Kill with a standard sigterm. * Linux only *
+-p <pid-file>  | Write process pid to <pid-file> * Linux only *
+___
+
+== Where to find more info
+Try its http://adchpp.sf.net/[home page] or the
+http://dcplusplus.sf.net/[DC++ home page].
+
+== Patches and contributions
+I'll gladly accept patches, but in order to avoid future licensing issues, I ask you to
+give me copyright over any submitted code. Make sure that the code doesn't break support
+for any of the platforms supported and that it looks more or less like the
+rest of the code (indent, names etc).
+
+Patches should be sent to the dcplusplus-devel mailing list. Subscription
+information can be found 
+https://lists.sourceforge.net/lists/listinfo/dcplusplus-devel[here].
+
+Please use unified patches agains latest svn trunk (i e svn diff) and supply a
+description of what the patch does.
+
+== Donations
+
+If you feel like helping out but don't know how, this is obviously a good way
+=)
+
+https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=arnetheduck%40gmail%2ecom&item_name=DCPlusPlus&no_shipping=1&return=http%3a%2f%2fdcplusplus%2esf%2enet%2f&cancel_return=http%3a%2f%2fdcplusplus%2esf%2enet%2f&cn=Greeting&tax=0&currency_code=EUR&bn=PP%2dDonationsBF&charset=UTF%2d8[Donate!]
+
+// vim: set syntax=asciidoc:
+
---
+++ trunk/TestingBeforeRelease/ConnectToHub.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FlowLib.Containers;
+using FlowLib.Connections;
+using FlowLib.Interfaces;
+using System.Threading;
+using FlowLib.Events;
+using FlowLib.Containers.Security;
+
+namespace TestingBeforeRelease
+{
+    [TestClass]
+    public class ConnectToHub
+    {
+        private bool _isFinished;
+        private bool _isConnected;
+        private int _regMode = -1;
+        private HubSetting _settings;
+
+        [TestMethod]
+        public void YnHub_UsingAutoProtocol()
+        {
+            HubTest("Auto");
+
+            if (!_isConnected)
+                throw new AssertFailedException("Unable to connect to YnHub using Auto Protocol");
+            if (!_isFinished)
+                throw new AssertFailedException("Connection established but no valid handshake to YnHub using Auto Protocol");
+        }
+
+        [TestMethod]
+        public void YnHub_UsingNmdcProtocol()
+        {
+            HubTest("Nmdc");
+
+            if (!_isConnected)
+                throw new AssertFailedException("Unable to connect to YnHub using Nmdc Protocol");
+            if (!_isFinished)
+                throw new AssertFailedException("Connection established but no valid handshake to YnHub using Nmdc Protocol");
+        }
+
+        [TestMethod]
+        public void Adc_UsingAutoProtocol()
+        {
+            _settings.Address = "127.0.0.1";
+            _settings.Port = 2780;
+            HubTest("Auto");
+
+            if (!_isConnected)
+                throw new AssertFailedException("Unable to connect to Adchpp using Auto Protocol");
+            if (!_isFinished)
+                throw new AssertFailedException("Connection established but no valid handshake to Adchpp using Auto Protocol");
+        }
+
+        [TestMethod]
+        public void Adcs_UsingAdcsProtocol()
+        {
+            _settings.Address = "127.0.0.1";
+            _settings.Port = 2781;
+            HubTest("AdcSecure");
+
+            if (!_isConnected)
+                throw new AssertFailedException("Unable to connect to Adchpp using Adcs Protocol");
+            if (!_isFinished)
+                throw new AssertFailedException("Connection established but no valid handshake to Adchpp using Adcs Protocol");
+        }
+        [TestInitialize()]
+        public void Init()
+        {
+            Util.InitilizeAll();
+
+            _settings = new HubSetting();
+            _settings.Address = "127.0.0.1";
+            _settings.Port = 411;
+            _settings.DisplayName = "FlowLib";
+        }
+
+        [TestCleanup()]
+        public void CleanUp()
+        {
+            _isFinished = false;
+            _regMode = -1;
+            _isConnected = false;
+        }
+
+        private void HubTest(string protocol)
+        {
+            _settings.Protocol = protocol;
+
+            Hub hubConnection = new Hub(_settings);
+            hubConnection.ConnectionStatusChange += new FlowLib.Events.FmdcEventHandler(OnConnectionStatusChange);
+            hubConnection.SecureUpdate += new FmdcEventHandler(hubConnection_SecureUpdate);
+            Hub.RegModeUpdated += new FmdcEventHandler(Hub_RegModeUpdated);
+
+            hubConnection.Connect();
+
+            int i = 0;
+            while (!_isFinished && i++ < 20)
+            {
+                Thread.Sleep(100);
+            }
+
+            hubConnection.ConnectionStatusChange -= OnConnectionStatusChange;
+            hubConnection.Disconnect("Test time exceeded");
+            hubConnection.Dispose();
+        }
+
+        void hubConnection_SecureUpdate(object sender, FmdcEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case Actions.SecurityValidateRemoteCertificate:
+                    CertificateValidationInfo ct = e.Data as CertificateValidationInfo;
+                    if (ct != null)
+                    {
+                        ct.Accepted = true;
+                        e.Data = ct;
+                        e.Handled = true;
+                    }
+                    break;
+            }
+        }
+
+        void Hub_RegModeUpdated(object sender, FmdcEventArgs e)
+        {
+            Hub hub = sender as Hub;
+            _regMode = (int)e.Action;
+            if (_regMode >= 0)
+            {
+                _isFinished = true;
+
+            }
+        }
+        
+        void OnConnectionStatusChange(object sender, FlowLib.Events.FmdcEventArgs e)
+        {
+            switch (e.Action)
+            {
+                case TcpConnection.Connected:
+                    _isConnected = true;
+                    break;
+            }
+        }
+    }
+}
